7000ccdec0b09c90af06a2777760d4d31851f0d8
Numin <xnuminousx@gmail.com>
2020-07-25 18:02:43 -0500

    Merged config command files into 1 file

--- src/main/java/me/numin/spirits2/commands/ConfigCommand.java
+++ src/main/java/me/numin/spirits2/commands/ConfigCommand.java
@@ -1,26 +1,39 @@
 package me.numin.spirits2.commands;
 
+import com.projectkorra.projectkorra.Element;
+import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.Ability;
+import com.projectkorra.projectkorra.ability.ComboAbility;
 import com.projectkorra.projectkorra.ability.CoreAbility;
 import com.projectkorra.projectkorra.command.PKCommand;
+import me.numin.spirits2.Spirits2;
+import me.numin.spirits2.utils.SpiritElement;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.MemorySection;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 public class ConfigCommand extends PKCommand {
 
-    private List<String> pkAliases = Arrays.asList("projectkorra", "pk", "korra");
-    private List<String> spiritAliases = Arrays.asList("spirits", "sp", "s");
+    private final List<String> pkAliases = Arrays.asList("projectkorra", "pk", "korra");
+    private final List<String> spiritAliases = Arrays.asList("spirits", "sp", "s");
+
+    private CommandSender commandSender;
+    private String section = "";
 
     public ConfigCommand() {
         super("config", "/bending config <Plugin> <Ability>", "Shows the config values for an ability.", new String[] {"con", "config", "configs", "getconfig"});
     }
 
     @Override
-    public void execute(CommandSender commandSender, List<String> list) {
-        if (commandSender == null || list == null) return;
+    public void execute(CommandSender cmdSender, List<String> list) {
+        if (cmdSender == null || list == null) return;
+
+        this.commandSender = cmdSender;
 
         if (!commandSender.hasPermission("bending.command.config")) {
             commandSender.sendMessage(ChatColor.RED + "You do not have permission for this command.");
@@ -28,7 +41,7 @@ public class ConfigCommand extends PKCommand {
         }
 
         if (list.size() <= 1) {
-            commandSender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Invalid command format!");
+            commandSender.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Configuration Command");
             commandSender.sendMessage("Format: /b config <plugin> <ability>");
             commandSender.sendMessage("");
             commandSender.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Plugins & Aliases:");
@@ -42,13 +55,80 @@ public class ConfigCommand extends PKCommand {
 
         String pluginFrom = list.get(0);
 
-        if (pkAliases.contains(pluginFrom)) {
+        if (pkAliases.contains(pluginFrom) || spiritAliases.contains(pluginFrom)) {
             Ability ability = CoreAbility.getAbility(list.get(1));
-            new FetchPKConfigValues(commandSender, ability);
-
-        } else if (spiritAliases.contains(pluginFrom)) {
-            Ability ability = CoreAbility.getAbility(list.get(1));
-            new FetchSpiritsConfigValues(commandSender, ability);
+            fetchConfigValues(pluginFrom, ability);
+        } else {
+            commandSender.sendMessage(pluginFrom);
+            commandSender.sendMessage("Invalid plugin provided.");
         }
+    }
+
+    public void fetchConfigValues(String plugin, Ability ability) {
+        if (ability == null) {
+            commandSender.sendMessage("That is not a valid ProjectKorra or Spirits ability.");
+            return;
+        }
+
+        Element element = ability.getElement();
+        Object value;
+        String configPath;
+        String elementName;
+        Set<String> configKeys;
+
+        if (pkAliases.contains(plugin)) {
+            elementName = element instanceof Element.SubElement ?
+                    ((Element.SubElement)element).getParentElement().getName() :
+                    element.getName();
+            configPath = "Abilities." + elementName + "." + ability.getName();
+            configKeys = ProjectKorra.plugin.getConfig().getKeys(true);
+        } else {
+            elementName = getSpiritElementString(ability.getElement());
+            configPath = ability instanceof ComboAbility ?
+                    "Abilities." + elementName + ".Combo." + ability.getName() :
+                    "Abilities." + elementName + "." + ability.getName();
+            configKeys = Spirits2.getInstance().getConfig().getKeys(true);
+        }
+
+        commandSender.sendMessage(ability.getElement().getColor() + "" + ChatColor.UNDERLINE + "" + ChatColor.BOLD + ability.getName());
+        commandSender.sendMessage("");
+
+        for (String key : configKeys) {
+            if (key.contains(configPath)) {
+                String[] parts = key.split(Pattern.quote("."));
+
+                String label = parts[parts.length - 1];
+                value = pkAliases.contains(plugin) ?
+                        ProjectKorra.plugin.getConfig().get(key) :
+                        Spirits2.getInstance().getConfig().get(key);
+
+                if (value == null ||
+                        value instanceof MemorySection ||
+                        label.equalsIgnoreCase("Description") ||
+                        label.equalsIgnoreCase("Instructions"))
+                    continue;
+
+                if (parts.length >= 5) {
+                    String sectionName = parts[parts.length - 2];
+
+                    if (!section.equals(sectionName) && !sectionName.equals(ability.getName())) {
+                        section = sectionName;
+                        commandSender.sendMessage("");
+                        commandSender.sendMessage(ability.getElement().getColor() + "" + ChatColor.BOLD + sectionName);
+                    }
+                }
+                commandSender.sendMessage(ability.getElement().getColor() + label + ": " + value.toString());
+            }
+        }
+    }
+
+    private String getSpiritElementString(Element element) {
+        if (element.equals(SpiritElement.SPIRIT))
+            return "Neutral";
+        else if (element.equals(SpiritElement.LIGHT_SPIRIT))
+            return "LightSpirit";
+        else if (element.equals(SpiritElement.DARK_SPIRIT))
+            return "DarkSpirit";
+        else return null;
     }
 }
